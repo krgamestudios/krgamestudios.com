2a6169e80800462a641b36d4aecec21627dea9d3
Kayne <kayneruse@gmail.com>
2018-05-27 17:37:24 +1000

    Webpack and babel working

---
+++ public_html/src/index_dev.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import App from './app.jsx';
+
+//render
+ReactDOM.render(
+  <p>hello world</p>,
+  document.querySelector('#root')
+);
---
+++ public_html/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './src/index_dev.jsx',
+  output: {
+    path: __dirname,
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js$|\.jsx$)/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      }
+    ]
+  }
+};
