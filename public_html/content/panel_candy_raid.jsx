d56dc0df657695005c7ec46dd0b13b87444db13a
Kayne <kayneruse@gmail.com>
2018-07-11 22:29:34 +1000

    Slight tweak

--- public_html/content/panel_candy_raid.jsx
+++ public_html/content/panel_candy_raid.jsx
@@ -9,7 +9,7 @@ class PanelCandyRaid extends React.Component {
           <img src="/img/crtfpromo.png" className="responsive" />
           <div style={{paddingLeft: '5px'}}>
             <p>Milly Monka's Candy Factory is the biggest producer of candy in the whole world. So naturally, she set up traps and defenses to protect her factory from thieves. One such thief is Candy the witch - a powerful witch with a powerful sweet tooth.</p>
-            <p>Candy must make her way through each section of the factory - the gumdrop gardens, the lava kitchens and the frozen warehouses - to steal as much candy as possible!</p>
+            <p>Candy must make her way through each section of the factory - the gumdrop gardens, the lava kitchens and the frozen vaults - to steal as much candy as possible!</p>
             <p>We're aiming for an October release, but you can play the demo now!</p>
             <ul>
               <li><a href="/candyraid">Candy Raid: The Factory Demo</a></li>
